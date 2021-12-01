99718d346e2abf9522a299e2d6ccb5bfbd517058
AngelPiva <angelpiva6@gmail.com>
2021-12-01 09:23:37 -0800

    fixed tokenize

--- split_str.c
+++ split_str.c
@@ -1,25 +1,4 @@
 #include "main.h"
-/**
- * _strdup - duplicates a string
- * @s: string
- * Return: pointer to new string (z)
- */
-char *_strdup(const char *s)
-{
-	char *z;
-	int a;
-
-	if (!s)
-		return (NULL);
-	for (a = 0; s[a]; a++)
-		;
-	z = malloc(sizeof(char *) * (a + 1));
-	if (!z)
-		return (NULL);
-	for (a = 0; s[a]; a++)
-		z[a] = s[a];
-	return (z);
-}
 
 /**
  * split_str - splits string
@@ -32,6 +11,7 @@ char **split_str(char *str, char sep)
 	char **out;
 	char *token, *cpy;
 	int amnt = 1, c;
+
 
 	for (c = 0; str[c]; c++)
 	{
@@ -57,17 +37,4 @@ char **split_str(char *str, char sep)
 	}
 	free(cpy);
 	return (out);
-}
-
-/**
- * free_array - frees an array of strings
- * @arr: array
- */
-void free_array(char **arr)
-{
-	int a;
-
-	for (a = 0; arr[a]; a++)
-		free(arr[a]);
-	free(arr);
 }
