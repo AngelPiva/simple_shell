8507c6c4626eaa70f33270c37104f0aae5a14a04
AngelPiva <angelpiva6@gmail.com>
2021-12-01 09:27:46 -0800

    function to free array

---
+++ _free.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * free_array - frees an array of strings
+ * @arr: array
+ */
+void free_array(char **arr)
+{
+	int a;
+
+	if (!arr)
+		return;
+	for (a = 0; arr[a]; a++)
+	{
+		free(arr[a]);
+	}
+	free(arr);
+}
