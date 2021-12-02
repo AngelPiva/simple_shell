dc1153be0fb3549018a998eb127a84eafa029f74
AngelPiva <angelpiva6@gmail.com>
2021-12-02 10:21:02 -0800

    fixed header

--- main.h
+++ main.h
@@ -9,5 +9,9 @@ char **split_str(char *str, char sep);
 void free_array(char **arr);
 char *_strdup(const char *s);
 char *_getenv(const char *name);
+int _printenv(void);
+extern char **environ;
+int _strncmp(char *s1, char *s2, size_t n);
+int _strlen(char *s);
 
 #endif
