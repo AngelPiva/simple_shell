e9e2580504cea3255d8cbdf57730d540c004f9cc
Manu <manuela.riossosa@gmail.com>
2021-12-05 08:24:37 -0800

    renamed _side_functions.c | moved _strlen.c & _strncmp.c to _str_functions.c

--- _str_functions.c
+++ _str_functions.c
@@ -38,3 +38,37 @@ char *_strdup(const char *s)
 		z[a] = s[a];
 	return (z);
 }
+
+/**
+*_strlen - returns the length of a string
+*@s: pointer
+*Return: 0
+*/
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
+
+/**
+*_strncmp - compares two strings
+*@s1: string 1
+*@s2: string 2
+*@n: first n bytes
+*Return: t
+*/
+
+int _strncmp(char *s1, char *s2, size_t n)
+{
+	size_t r = 0, t = 0;
+
+	while (s1[r] == s2[r] && s1[r] != '\0' && s2[r] != '\0' && r < n)
+		r++;
+	if (s1[r] != s2[r])
+		t = s1[r] - s2[r];
+	return (t);
+}
