dba084da4708100a5ce4d7eb9c0786f8b8153094
Manu <manuela.riossosa@gmail.com>
2021-12-05 06:40:37 -0800

    renamed _env_things.c | moved _getenv from _side_functions.c to _env_functions.c

---
+++ _env_functions.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ *_printenv - prints all the environment variables
+ *Return: 0
+ */
+
+char _printenv(void)
+{
+	int n = 0;
+
+	while (environ[n])
+	{
+		write(1, environ[n], _strlen(environ[n]));
+		write(1, "\n", 2);
+		n++;
+	}
+	return (0);
+}
+
+/**
+ * _getenv - gets an environment variable
+ * @name: name of the env variable
+ * Return: pointer to the env varable
+ */
+
+char *_getenv(const char *name)
+{
+	int c, s, found;
+
+	if (!name)
+		return (NULL);
+	for (c = 0; environ[c]; c++)
+	{
+		found = 0;
+		for (s = 0; name[s]; s++)
+			if (name[s] != environ[c][s])
+			{
+				found = 1;
+				break;
+			}
+			if (!found)
+			break;
+	}
+	if (!environ[c])
+		return (NULL);
+	return (environ[c] + (s + 1));
+}
--- _side_functions.c
+++ _side_functions.c
@@ -38,32 +38,3 @@ char *_strdup(const char *s)
 		z[a] = s[a];
 	return (z);
 }
-
-/**
- * _getenv - gets an environment variable
- * @name: name of the env variable
- * Return: pointer to the env varable
- */
-
-char *_getenv(const char *name)
-{
-	int c, s, found;
-
-	if (!name)
-		return (NULL);
-	for (c = 0; environ[c]; c++)
-	{
-		found = 0;
-		for (s = 0; name[s]; s++)
-			if (name[s] != environ[c][s])
-			{
-				found = 1;
-				break;
-			}
-		if (!found)
-			break;
-	}
-	if (!environ[c])
-		return (NULL);
-	return (environ[c] + (s + 1));
-}
